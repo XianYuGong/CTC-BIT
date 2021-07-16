590c59c83e1fcbe1be787b46b0805a9b6370864c
XianYuGong <gongpeiyuan1@163.com>
2021-07-16 14:00:24 +0800

    Add BERT-CRF

---
+++ GED/models/bert_for_ged.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from crf import CRF
+from transformers import BertModel,BertPreTrainedModel
+
+
+
+
+
+
+class BertCrfForGED(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertCrfForGED, self).__init__(config)
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.crf = CRF(num_tags=config.num_labels, batch_first=True)
+        self.init_weights()
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None,labels=None):
+        outputs =self.bert(input_ids = input_ids,attention_mask=attention_mask,token_type_ids=token_type_ids)
+        sequence_output = outputs[0]
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output)
+        outputs = (logits,)
+        if labels is not None:
+            loss = self.crf(emissions = logits, tags=labels, mask=attention_mask)
+            outputs =(-1*loss,)+outputs
+        return outputs # (loss), scores
